3b0b564485db143d5c503c055f2993718b7729c7
Fernando Aguero <fernandoaes20@gmail.com>
2022-11-12 22:46:21 -0300

    dialect añadido

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const { DB_PASS, DB_USER, DB_DATABASE, DB_HOST, DB_DIALECT } = require('./config
 
 const dataBase = new Sequelize(DB_DATABASE, DB_USER, DB_PASS, {
   host: DB_HOST,
-  dialect: DB_DIALECT,
+  dialect: 'postgres',
   logging: false
 })
 
